6a7b894b404646265ffb7a5d966d2f7f6e7c1476
HungTrinhIT <minhhung.it.work@gmail.com>
2023-07-19 20:37:13 +0700

    lesson 01 , nodejs overview

---
+++ lesson-01-overview/math.js
@@ -0,0 +1,10 @@
+const sum = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+
+module.exports = {
+  sumFunction: sum,
+  multiply,
+};
+
+// module.exports.sumFn = sum;
+// module.exports.mulFn = multiply;
