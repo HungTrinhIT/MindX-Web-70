fa8e18d36ad4dcbad85b70fbcc19e0e757dd47e5
HungTrinhIT <minhhung.it.work@gmail.com>
2023-08-13 19:24:29 +0700

    update

--- lesson07-common-practices/configs/db.config.js
+++ lesson07-common-practices/configs/db.config.js
@@ -4,8 +4,8 @@ const MONGO_URI = "mongodb://0.0.0.0:27017/mindx-web70-social-apps";
 
 export const connectToDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log("Database connected");
+    const connection = await mongoose.connect(MONGO_URI);
+    console.log(`Database connected at ${connection.connection.host}`);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
