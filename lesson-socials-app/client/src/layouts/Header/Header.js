02ab494e11d4b38d29994271fed744c0fc0c11b2
HungTrinhIT <minhhung.it.work@gmail.com>
2023-08-16 19:11:28 +0700

    add tailwindcss

--- lesson-socials-app/client/src/layouts/Header/Header.js
+++ lesson-socials-app/client/src/layouts/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext/AuthContext";
 
 const Header = () => {
@@ -12,23 +12,58 @@ const Header = () => {
     handleLogOut();
     navigate("/login");
   };
+
+  const baseNavItemClass = " hover:text-gray-500 px-4 py-2 rounded-md";
   return (
-    <header>
-      <h3>Facebook</h3>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Sign up</Link>
-          </li>
-          {isAuthenticated && <button onClick={onLogoutHandler}>Logout</button>}
-        </ul>
-      </nav>
+    <header className="bg-blue-400 py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h3 className="text-white font-bold text-lg">Facebook</h3>
+        <nav>
+          <ul className="flex items-center gap-2">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${baseNavItemClass} text-gray-500`
+                    : `${baseNavItemClass} text-white`
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/login"
+                activeClassName="text-gray-500"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${baseNavItemClass} text-gray-500`
+                    : `${baseNavItemClass} text-white`
+                }
+              >
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                activeClassName="text-gray-500"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${baseNavItemClass} text-gray-500`
+                    : `${baseNavItemClass} text-white`
+                }
+              >
+                Sign up
+              </NavLink>
+            </li>
+            {isAuthenticated && (
+              <button onClick={onLogoutHandler}>Logout</button>
+            )}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
