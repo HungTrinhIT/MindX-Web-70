f823f73339533c5f381a869cdfac778eba5f34e9
HungTrinhIT <minhhung.it.work@gmail.com>
2023-08-30 19:15:22 +0700

    update

--- social-app-be/controllers/posts.controller.js
+++ social-app-be/controllers/posts.controller.js
@@ -30,6 +30,7 @@ const createNewPost = asyncHandler(async (req, res) => {
 		dat: post,
 	});
 });
+
 const getAllPosts = asyncHandler(async (req, res) => {
 	const page = parseInt(req.query.page) || 1;
 	const limit = parseInt(req.query.limit) || 10;
@@ -37,23 +38,90 @@ const getAllPosts = asyncHandler(async (req, res) => {
 
 	const postsWithPagination = await PostModel.find().skip(skip).limit(limit);
 	const totalPosts = await PostModel.countDocuments();
-	const totalPages = await Math.ceil(totalPosts / limit);
+	const totalPages = Math.ceil(totalPosts / limit);
 
 	return res.json({
 		data: postsWithPagination,
 		pagination: {
-			totalCount: totalPosts,
-			pageCount: postsWithPagination.length,
-			page: page,
 			totalPages,
-			perPage: limit,
+			totalCount: totalPosts,
+			page: page,
+			pageSize: limit,
 		},
 	});
 });
 
-const getSingle = asyncHandler(async (req, res) => {});
-const getAllPostsByUser = asyncHandler(async (req, res) => {});
-const updatePost = asyncHandler(async (req, res) => {});
+const getAllPostsByUser = asyncHandler(async (req, res) => {
+	const userId = req.user.id;
+
+	const currentUser = await UserModel.findById(userId);
+
+	if (!currentUser) {
+		res.status(400);
+		throw new Error('User not found');
+	}
+
+	const page = parseInt(req.query.page) || 1;
+	const limit = parseInt(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	const totalPosts = await PostModel.countDocuments();
+	const totalPages = Math.ceil(totalPosts / limit);
+
+	const posts = await PostModel.find({ user: userId }).skip(skip).limit(limit);
+
+	return res.json({
+		data: posts,
+		pagination: {
+			totalPages,
+			totalCount: totalPosts,
+			page,
+			pageSize: limit,
+		},
+	});
+});
+const getSingle = asyncHandler(async (req, res) => {
+	const postId = req.params.id;
+
+	const post = await PostModel.findById(postId).populate(
+		'user',
+		'fullname email id',
+	);
+
+	if (!post) {
+		res.status(400);
+		throw new Error('Post not found');
+	}
+
+	res.json(post);
+});
+
+const updatePost = asyncHandler(async (req, res) => {
+	const postId = req.params.id;
+	const { title, content } = req.body;
+	const userId = req.user.id;
+
+	const post = await PostModel.findById(postId);
+
+	if (!post) {
+		res.status(400);
+		throw new Error('Post not found');
+	}
+
+	if (post.user.toString() !== userId) {
+		res.status(403);
+		throw new Error('Unauthorized access');
+	}
+
+	post.title = title;
+	post.content = content;
+	post.updatedAt = new Date();
+
+	const updatedPost = await post.save();
+
+	res.json(updatedPost);
+});
+
 const deletePost = asyncHandler(async (req, res) => {});
 
 const PostController = {
